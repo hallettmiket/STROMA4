3c165abb65beb6a6c4f1cf7c08385ffed3c66875
Sadiq Saleh <sadiq.saleh@gmail.com>
2016-11-09 15:59:26 -0500

    Added missing backslashes to R files before running document(). Updated version number.

--- R/BL2.property.R
+++ R/BL2.property.R
@@ -2,9 +2,9 @@
 #' 
 #' This genelist is used to assign the related property using the assign.property function
 #' @format A dataframe with 2 columns, and 291 rows
-#'  describe{
-#'    item Gene.Symbol ID of transcript (HGNC where available)
-#'    item Direction Direction of expression (up/down)
+#' \describe{
+#'   \item{Gene.Symbol}{ID of transcript (HGNC where available)}
+#'   \item{Direction}{Direction of expression (up/down)}
 #' }
 #' @source Identified in related publication
 'BL2.property'
--- R/LAR.property.R
+++ R/LAR.property.R
@@ -2,9 +2,9 @@
 #' 
 #' This genelist is used to assign the related property using the assign.property function
 #' @format A dataframe with 2 columns, and 1205 rows
-#'  describe{
-#'    item Gene.Symbol ID of transcript (HGNC where available)
-#'    item Direction Direction of expression (up/down)
+#' \describe{
+#'   \item{Gene.Symbol}{ID of transcript (HGNC where available)}
+#'   \item{Direction}{Direction of expression (up/down)}
 #' }
 #' @source Identified in related publication
 'LAR.property'
